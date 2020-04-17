04df8732e6b183f15d5742a75c35e20363b541d6
kulkara <pradeep_lokhande@protonmail.com>
2020-04-17 17:41:49 +0530

    Commit everything

---
+++ CleanArch/CleanArch.Mvc/Controllers/Course/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArch.Mvc.Controllers.Course
+{
+    //[Authorize]
+    //This attribute will force user to login so that only vaided user will be able to see Courses
+    public class CourseController : Controller
+    {
+
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+          CourseViewModel model1 =  _courseService.GetCourses();
+            return View(model1);
+        }
+    }
+}
