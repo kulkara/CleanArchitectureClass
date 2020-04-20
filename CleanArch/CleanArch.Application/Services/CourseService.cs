3c29fe5211597a17745b30b2133834bf2c9b99a8
kulkara <pradeep_lokhande@protonmail.com>
2020-04-20 13:26:13 +0530

    Commit

--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -3,23 +3,45 @@ using System.Collections.Generic;
 using System.Text;
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Core.Commands;
 using CleanArch.Domain.Interfaces;
 
 namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
-        ICourseRepository _courseRepository;
+       
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus )
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
         public CourseViewModel GetCourses()
         {
             CourseViewModel model = new CourseViewModel();
             model.Courses = _courseRepository.GetCourses();
             return  model; 
+        }
+
+        void ICourseService.Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                courseViewModel.Name, 
+                courseViewModel.Description, courseViewModel.ImageUrl
+                );
+            _bus.SendCommand(createCourseCommand);
+
+
+        }
+
+        CourseViewModel ICourseService.GetCourses()
+        {
+            throw new NotImplementedException();
         }
     }
 }
